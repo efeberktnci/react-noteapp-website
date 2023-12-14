f73422b409fe661b611f452a33ffb1c5e4f71618
efeberktnci <efeberktanci@gmail.com>
2023-12-14 07:22:46 +0300

    Added edit section functionality

--- src/pages/EditNote.jsx
+++ src/pages/EditNote.jsx
@@ -1,43 +1,67 @@
-import React from 'react'
-import { useState } from 'react'
-import { IoIosArrowBack } from 'react-icons/io'
-import { Link , useParams} from 'react-router-dom'
-import {MdDelete} from "react-icons/md"
+import React from 'react';
+import { useState } from 'react';
+import { IoIosArrowBack } from 'react-icons/io';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+import { MdDelete } from 'react-icons/md';
+import useCreateDate from '../components/useCreateDate';
 
-const EditNote = ({notes,setNotes}) => {
-  const [title, setTitle] = useState("")
-  const [details, setDetails] = useState("")
+const EditNote = ({ notes, setNotes }) => {
   const { id } = useParams();
-  const note = notes.find((item)=> item.id == id)
+  const note = notes.find((item) => item.id == id);
+
+  // Check if 'note' is defined before using its properties
+  const [title, setTitle] = useState(note ? note.title : '');
+  const [details, setDetails] = useState(note ? note.details : '');
   
+  const date = useCreateDate();
+  const navigate = useNavigate();
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    if (title && details) {
+      const newNote = { ...note, title, details, date };
+
+      const newNotes = notes.map((item) => {
+        if (item.id == id) {
+          item = newNote;
+        }
+        return item;
+      });
+      setNotes(newNotes);
+    }
+    navigate('/');
+  };
+
   return (
     <section>
       <header className='create-note__header'>
-        <Link className='btn' to="/">
-        <IoIosArrowBack/>
+        <Link className='btn' to='/'>
+          <IoIosArrowBack />
         </Link>
-        <button className='btn lg primary'>Kaydet</button>
+        <button className='btn lg primary' onClick={handleForm}>
+          Kaydet
+        </button>
         <button className='btn lg danger'>
-          <MdDelete/>
+          <MdDelete />
         </button>
       </header>
-      <form className='create-note__form'>
-        <input 
-        type="text"
-         placeholder='Başlık'
-         autoFocus
+      <form className='create-note__form' onSubmit={handleForm}>
+        <input
+          type='text'
+          placeholder='Başlık'
+          autoFocus
           value={title}
-          onChange={(e)=>setTitle(e.target.value)}
-          />
-        <textarea  
-        rows="28" 
-        placeholder="Not Detayı..." 
-        value={details} 
-        onChange={(e)=>setDetails(e.target.value)} >
-        </textarea>
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <textarea
+          rows='28'
+          placeholder='Not Detayı...'
+          value={details}
+          onChange={(e) => setDetails(e.target.value)}
+        ></textarea>
       </form>
     </section>
-  )
-}
+  );
+};
 
-export default EditNote
+export default EditNote;
